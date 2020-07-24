a20e9f7e1cdb28bcaf287d0e4ee0b283a12e6ec7
Henry Faulkner <henry.faulkner@perficient.com>
2020-07-24 15:20:07 -0400

    personal, postinternship commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ADO_Practical2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstTextBox = new System.Windows.Forms.TextBox();
+            this.lastTextBox = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.findBtn = new System.Windows.Forms.Button();
+            this.majorDrop = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstTextBox
+            // 
+            this.firstTextBox.Location = new System.Drawing.Point(170, 72);
+            this.firstTextBox.Name = "firstTextBox";
+            this.firstTextBox.Size = new System.Drawing.Size(121, 22);
+            this.firstTextBox.TabIndex = 0;
+            // 
+            // lastTextBox
+            // 
+            this.lastTextBox.Location = new System.Drawing.Point(170, 135);
+            this.lastTextBox.Name = "lastTextBox";
+            this.lastTextBox.Size = new System.Drawing.Size(121, 22);
+            this.lastTextBox.TabIndex = 1;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(536, 120);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 2;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(536, 166);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(75, 23);
+            this.updateBtn.TabIndex = 3;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(536, 217);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 4;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // findBtn
+            // 
+            this.findBtn.Location = new System.Drawing.Point(536, 72);
+            this.findBtn.Name = "findBtn";
+            this.findBtn.Size = new System.Drawing.Size(75, 23);
+            this.findBtn.TabIndex = 5;
+            this.findBtn.Text = "Find";
+            this.findBtn.UseVisualStyleBackColor = true;
+            this.findBtn.Click += new System.EventHandler(this.findBtn_Click);
+            // 
+            // majorDrop
+            // 
+            this.majorDrop.FormattingEnabled = true;
+            this.majorDrop.Location = new System.Drawing.Point(170, 193);
+            this.majorDrop.Name = "majorDrop";
+            this.majorDrop.Size = new System.Drawing.Size(121, 24);
+            this.majorDrop.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Major";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.majorDrop);
+            this.Controls.Add(this.findBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.lastTextBox);
+            this.Controls.Add(this.firstTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstTextBox;
+        private System.Windows.Forms.TextBox lastTextBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button findBtn;
+        private System.Windows.Forms.ComboBox majorDrop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_Practical2
+{
+    public partial class Form1 : Form
+    {
+        private Fabrikam_ADOEntities context = new Fabrikam_ADOEntities();
+        private string[] majorsArr = new string[5] { "English", "Computer Science", "MIS", "Physics", "Mathematics" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            majorDrop.Items.AddRange(majorsArr); //adds Major options to ComboBox
+        }
+
+        private void findBtn_Click(object sender, EventArgs e)
+        {
+            string firstName = firstTextBox.Text;
+            string lastName = lastTextBox.Text;
+
+            var query =
+                from c in context.Students
+                where c.FirstName == firstName && c.LastName == lastName
+                select c;
+
+            if(!query.Any()) MessageBox.Show("This student does not exist in our system.");
+
+            foreach (var obj in query)
+            {
+                if (obj.Major != null)
+                {
+                    MessageBox.Show(
+                        $"{firstName} {lastName} successfully found. Their Id is {obj.Id} and Major is {obj.Major}.");
+                }
+                else
+                {
+                    MessageBox.Show(
+                        $"{firstName} {lastName} successfully found. Their Id is {obj.Id} and Major is unidentified.");
+                }
+            }
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            string firstName = firstTextBox.Text;
+            string lastName = lastTextBox.Text;
+            string major = null;
+            if (majorDrop.SelectedItem != null)
+            {
+                major = majorDrop.SelectedItem.ToString();
+            }
+
+            var student = new Student
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Major = major
+            };
+
+            context.Students.Add(student);
+            try
+            {
+                context.SaveChanges();
+                MessageBox.Show("Insert Successful.");
+            }
+            catch (DbUpdateException ex) //Weird error. once duplicate entry tried, all further attempt receive error
+            {
+                Console.WriteLine(ex.ToString());
+                MessageBox.Show("Insert Unsuccessful. Duplicate Entry.");
+            }
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            string firstName = firstTextBox.Text;
+            string lastName = lastTextBox.Text;
+
+            var query =
+                from c in context.Students
+                where c.FirstName == firstName && c.LastName == lastName
+                select c;
+
+            if (!query.Any())
+            {
+                MessageBox.Show("This student does not exist in our system.");
+            }
+            else
+            {
+                MessageBox.Show("Update Successful.");
+            }
+
+            foreach (var obj in query)
+            {
+                obj.Major = majorDrop.SelectedItem.ToString();
+            }
+
+            context.SaveChanges();
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            string firstName = firstTextBox.Text;
+            string lastName = lastTextBox.Text;
+
+            var query =
+                from c in context.Students
+                where c.FirstName == firstName && c.LastName == lastName
+                select c;
+
+            if (!query.Any())
+            {
+                MessageBox.Show("This student does not exist in our system.");
+            }
+            else
+            {
+                MessageBox.Show("Update Successful.");
+            }
+
+            foreach (var obj in query)
+            {
+                context.Students.Remove(obj);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
